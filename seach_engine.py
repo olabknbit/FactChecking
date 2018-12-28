54429f63cd31580e40eb7a100c3a4377b49d5131
Aleksandra <al.byczynska@gmail.com>
2018-12-28 22:58:50 +0100

    add fix snippets, scrape web for question snippets

--- seach_engine.py
+++ seach_engine.py
@@ -75,7 +75,7 @@ def write_snippets_to_file(mode, start_from=None):
                 query = last_question + ' ' + text
                 queries.append((tag, query))
 
-    with open('data/b/b-factual-q-a-web-results-' + mode + '.txt', 'a') as f:
+    with open('data/b/b-factual-q-a-web-results-' + mode + '.txt', 'w') as f:
         for tag, query in queries:
             if works:
                 snippets = get_scrapes(query)
@@ -84,9 +84,77 @@ def write_snippets_to_file(mode, start_from=None):
                 works = True
 
 
+def fix_snippets(mode):
+    with open('data/b/b-factual-q-a-clean-' + mode + '.txt', 'r') as f:
+        lines = f.readlines()
+        last_question = ''
+        queries = []
+        for line in lines:
+            prts = line.split('\t')
+            tag = prts[0].strip()
+            typ = prts[1]
+            text = prts[2].strip()
+            if typ == 'NA':
+                last_question = text
+            else:
+                query = last_question + ' ' + text
+                queries.append((tag, query))
+
+    save_filename = 'data/b/b-factual-q-a-web-results-' + mode + '.txt'
+    with open(save_filename + '.copy', 'r') as f:
+        lines = f.readlines()
+        dones = []
+        for line in lines:
+            prts = line.split('\t')
+
+            if len(prts) == 1:
+                tag = prts[0].strip()
+                dones.append((tag, None))
+            if len(prts) > 1:
+                tag = prts[0].strip()
+                text = prts[1].strip()
+                dones.append((tag, text))
+
+    with open(save_filename, 'w') as f:
+        for q_row, done_row in zip(queries, dones):
+            q_tag, query = q_row
+            d_tag, snippets = done_row
+            if q_tag != d_tag:
+                print("OOHH", q_tag, d_tag, len(q_tag), len(d_tag))
+                exit(1)
+            # if snippets == '':
+            #     snippets = get_scrapes(query) + '\n'
+            # print(snippets)
+            if snippets is None:
+                snippets = get_scrapes(query)
+            f.write(q_tag + '\t' + snippets + '\n')
+
+
+def scrape_questions(mode):
+    with open('data/b/b-factual-q-a-clean-' + mode + '.txt', 'r') as f:
+        lines = f.readlines()
+        queries = []
+        for line in lines:
+            prts = line.split('\t')
+            tag = prts[0]
+            typ = prts[1]
+            text = prts[2]
+            if typ == 'NA':
+                query = text
+                queries.append((tag, query))
+
+    with open('data/b/b-factual-q-web-results-' + mode + '.txt', 'w') as f:
+        for tag, query in queries:
+            snippets = get_scrapes(query)
+            f.write(tag + '\t' + snippets + '\n')
+
+
 if __name__ == '__main__':
     import nltk
 
     nltk.download('stopwords')
-    write_snippets_to_file('test')
-    write_snippets_to_file('train')
+    # write_snippets_to_file('test')
+    # write_snippets_to_file('train')
+    # fix_snippets('train')
+    scrape_questions('train')
+    scrape_questions('test')
